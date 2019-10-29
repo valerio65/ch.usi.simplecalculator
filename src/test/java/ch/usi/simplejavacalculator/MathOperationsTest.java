eb21348ad7bbf61ec8de14fc7baba2a1d0b4561b
Valerio <valerio.terragni@usi.ch>
2019-10-29 10:31:35 +0100

    initial version

--- src/test/java/ch/usi/simplejavacalculator/MathOperationsTest.java
+++ src/test/java/ch/usi/simplejavacalculator/MathOperationsTest.java
@@ -7,15 +7,6 @@ import static org.junit.Assert.*;
 public class MathOperationsTest {
 
 
-    @Test
-    public void testMinus1(){
-// sum 10 - 5
-        Double expected = 5.0;
-        Double actual = MathOperations.minus(10.0,5.0);
-        assertEquals(expected,actual,0.00000000001);
-    }
-
-
 
 
 
